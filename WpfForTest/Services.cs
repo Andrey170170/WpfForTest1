fdff56f99871ab925fba07a79141ed265c352a14
Андрей Копанев <24122004@bk.ru>
2022-05-22 14:07:23 +0300

    Add project files.

---
+++ WpfForTest/Services.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfForTest
+{
+    public class Services
+    {
+        public Services (string coverImage, string title)
+        {
+            CoverImage = coverImage;
+            Title = title;
+        }
+
+        public string CoverImage { get; set; }
+        public string Title { get; set; }
+    }
+}
