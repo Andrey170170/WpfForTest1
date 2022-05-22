e3b4f6d6519036def9b31aea0f6f92f2ff4d6ad6
Андрей Копанев <24122004@bk.ru>
2022-05-22 14:07:23 +0300

    Add project files.

---
+++ WpfForTest/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfForTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<string> styles = new List<string> { "light", "dark" };
+            styleList.SelectionChanged += ThemeChanged;
+            styleList.ItemsSource = styles;
+            styleList.SelectedItem = "light";
+        }
+
+        private void ThemeChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string? style = styleList.SelectedItem as string;
+            // определяем путь к файлу ресурсов
+            var uri = new Uri(style?.ToLower() + ".xaml", UriKind.Relative);
+            // загружаем словарь ресурсов
+            ResourceDictionary? resourceDict = Application.LoadComponent(uri) as ResourceDictionary;
+            // очищаем коллекцию ресурсов приложения
+            Application.Current.Resources.Clear();
+            // добавляем загруженный словарь ресурсов
+            Application.Current.Resources.MergedDictionaries.Add(resourceDict);
+        }
+
+        private void Service_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(@"Услуги");
+        }
+        private void AboutCompany_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(@"О компании");
+        }
+        private void OurWorks_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(@"Наши работы");
+        }
+        private void FindUs_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(@"Как найти нас ");
+        }
+    }
+}
